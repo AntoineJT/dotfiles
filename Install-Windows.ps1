f798ee1948e23be645c37915ca67fb7bc1ef2d38
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2024-07-14 00:21:25 +0200

    improvement

--- Install-Windows.ps1
+++ Install-Windows.ps1
@@ -2,6 +2,7 @@
 # TODO ask for image to use for winfetch
 # TODO copy files in <sysname> to home directory
 
+$WINTERM_QUAKE_MODE_KEY = "²"
 $WINTERM_SETTINGS_PATH = "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
 # install git (required for NerdFonts)
@@ -10,23 +11,26 @@ winget install Git.Git
 
 # install nerdfonts
 Write-Output "Installing NerdFonts version of Cascadia Code & Cascadia Mono..."
-cd cache
+Set-Location cache
 if (!(Test-Path -Path ./nerd-fonts)) {
     git clone --depth 1 https://github.com/ryanoasis/nerd-fonts nerd-fonts
 } else {
     Write-Output "NerdFonts git already cloned. If you want to update it, do it manually."
 }
-cd nerd-fonts
+Set-Location nerd-fonts
 ./install.ps1 CascadiaCode, CascadiaMono
-cd ../..
+Set-Location ../..
 
 # install Oh My Posh
+Write-Output "Installing Oh My Posh..."
 winget install JanDeDobbeleer.OhMyPosh -s winget
 
 # install Terminal-Icons
+Write-Output "Installing Terminal Icons..."
 Install-Module -Name Terminal-Icons -Scope CurrentUser
 
 # install winfetch
+Write-Output "Installing winfetch..."
 Install-Script -Name winfetch -Scope CurrentUser
 
 # configure windows terminal
@@ -38,12 +42,12 @@ $settingsJson.profiles.defaults = @{
         "face" = "CaskaydiaCove Nerd Font Mono"
     }
 }
-# setup quake mode on ² key press
+# setup quake mode activation on given key press
 $settingsJson.actions += @{
     "command" = @{
         "action" = "globalSummon"
         "name" = "_quake"
     }
-    "keys" = "²"
+    "keys" = $WINTERM_QUAKE_MODE_KEY
 }
 $settingsJson | ConvertTo-Json -Depth 32 | Set-Content $WINTERM_SETTINGS_PATH
