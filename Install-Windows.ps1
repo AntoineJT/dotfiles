0498c83dbf11ae5c08541fa388d240becfedf5b3
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2024-07-13 22:28:30 +0200

    configure windows terminal to use nerdfonts

--- Install-Windows.ps1
+++ Install-Windows.ps1
@@ -1,8 +1,9 @@
-# TODO configure terminal to use nerdfonts
 # TODO configure terminal for quake mode using ² key as trigger
 # TODO ask for terminal root dir and set it in the environment variable
 # TODO ask for image to use for winfetch
 # TODO copy files in <sysname> to home directory
+
+$WINTERM_SETTINGS_PATH = "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
 # install git (required for NerdFonts)
 Write-Output "Installing Git..."
@@ -28,3 +29,14 @@ Install-Module -Name Terminal-Icons -Scope CurrentUser
 
 # install winfetch
 Install-Script -Name winfetch -Scope CurrentUser
+
+# configure windows terminal
+Write-Output "Configuring Windows Terminal..."
+$settingsJson = Get-Content $WINTERM_SETTINGS_PATH -Raw | ConvertFrom-Json
+# set NerdFonts' Cascadia Code as Windows Terminal font
+$settingsJson.profiles.defaults = @{
+    "font" = @{
+        "face" = "CaskaydiaCove Nerd Font Mono"
+    }
+}
+$settingsJson | ConvertTo-Json -Depth 32 | Set-Content $WINTERM_SETTINGS_PATH
