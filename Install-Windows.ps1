39554d1c689b4e59eece648800338d41a242854e
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2024-07-13 17:38:00 +0200

    install nerdfonts (and git as dependency)

--- Install-Windows.ps1
+++ Install-Windows.ps1
@@ -1,7 +1,23 @@
-# todo install nerdfonts
-# todo configure terminal to use nerdfonts
-# todo configure terminal for quake mode using ² key as trigger
-# todo ask for terminal root dir and set it in the environment variable
+# TODO configure terminal to use nerdfonts
+# TODO configure terminal for quake mode using ² key as trigger
+# TODO ask for terminal root dir and set it in the environment variable
+# TODO ask for image to use for winfetch
+# TODO copy files in <sysname> to home directory
+
+# install git (required for NerdFonts)
+Write-Output "Installing Git..."
+winget install Git.Git
+
+# install nerdfonts
+Write-Output "Installing NerdFonts version of Cascadia Code & Cascadia Mono..."
+cd cache
+if (!(Test-Path -Path ./nerd-fonts)) {
+    Write-Output "NerdFonts git already cloned. If you want to update it, do it manually."
+    git clone --depth 1 https://github.com/ryanoasis/nerd-fonts nerd-fonts
+}
+cd nerd-fonts
+./install.ps1 CascadiaCode, CascadiaMono
+cd ../..
 
 # install Oh My Posh
 winget update
