0bf4d7970962f5ea8aee0bf107e86c8f4dbcc514
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2024-09-09 19:01:45 +0200

    Improve PS_STARTUP_DIR handling

--- Windows/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -14,8 +14,12 @@ Import-Module -Name Terminal-Icons
 # oh my posh with half-life theme
 oh-my-posh init pwsh --config $Env:POSH_THEMES_PATH/half-life.omp.json | Invoke-Expression
 
-# change directory to terminal root dir
-Set-Location -Path $Env:PS_STARTUP_DIR
+# use terminal root dir as default
+# directory (ignore it when opened
+# in a specific dir)
+if ((Get-Location).Path -eq $HOME) {
+    Set-Location -Path $Env:PS_STARTUP_DIR
+}
 
 # aliases
 Set-Alias -Name lum -Value Set-Screen-Brightness
